55b5215728bc85a7fb33bd37868b830e0ddbadf7
Bae Sujeong <doya60@gmail.com>
2022-05-18 00:26:07 +0900

    탑버튼, 보더 애니메이션 추가

---
+++ js/main.js
@@ -0,0 +1,185 @@
+const topBtn = document.querySelector("#topBtn");
+const newsArticles = document.querySelectorAll("#news article");
+
+const _top = document.querySelectorAll(".borderTop");
+const _right = document.querySelectorAll(".borderRight");
+const _bottom = document.querySelectorAll(".borderBottom");
+const _left = document.querySelectorAll(".borderLeft");
+const speed = 100;
+// const _unHover = [];
+
+window.addEventListener("scroll", ()=>{
+  if(this.scrollY > 900) {
+    topBtn.classList.add("on");
+  } else {
+    topBtn.classList.remove("on");
+  }
+})
+
+topBtn.addEventListener("click",e=>{
+  e.preventDefault();
+
+  new Anime(window,{
+    prop:"scroll",
+    value:0,
+    duration:300
+  })
+})
+
+// let borderContent = '';
+
+// borderContent += `
+//   <div class="top"></div>
+//   <div class="right"></div>
+//   <div class="bottom"></div>
+//   <div class="left"></div>
+// `;
+
+// for(const el of newsArticleBorder){
+//   el.innerHTML = borderContent;
+// }
+
+newsArticles.forEach((el,index)=>{
+  // _unHover.push(false);
+
+  el.addEventListener("mouseenter",()=>{
+    // _unHover[index] = false;
+
+    borderDraw(index);
+  })
+
+  el.addEventListener("mouseleave",()=>{
+    // if(_unHover[index] == false)
+    //   _unHover[index] = true;
+
+    borderRemove(index);
+  })
+})
+
+// function ani(_dir, _idx, _prop, _value, _call) {
+//   // if(on == false)
+//   //   return;
+//   if(_unHover[_idx] == true){
+    
+//     borderRemove(_idx);
+//     return;
+//   }
+
+//   new Anime(_dir[_idx], {
+//     prop: _prop,
+//     value: _value,
+//     callback: _call
+//   })
+
+//   // if(_call == null)
+//   // {
+//   //   new Anime(_dir, {
+//   //     prop: _prop,
+//   //     value: _value
+//   //   })
+//   // }
+//   // else 
+//   // {
+//   //   new Anime(_dir, {
+//   //     prop: _prop,
+//   //     value: _value,
+//   //     callback: _call
+//   //   })
+//   // }
+// }
+
+// function borderDraw(index){
+//   newsArticles.forEach((el,idx)=>{
+//     if (index != idx){
+//       borderRemove(idx);
+//     }
+//   })
+
+//   ani(_top,
+//       index, 
+//       "width", 
+//       "100%", 
+//       () => ani(_right,
+//                 index, 
+//                 "height",
+//                 "100%",
+//                 () => ani(_bottom,     
+//                           index, 
+//                           "width",
+//                           "100%",
+//                           () => ani(_left,
+//                                     index, 
+//                                     "height",
+//                                     "100%"))));
+// }
+
+
+
+function borderDraw(index){
+  newsArticles.forEach((el,idx)=>{
+    if (index != idx)
+      borderRemove(idx);
+  })
+
+  new Anime(_top[index],{
+    prop:"width",
+    value:"100%",
+    duration: speed,
+    callback:()=>{
+      new Anime(_right[index],{
+          prop:"height", 
+          value:"100%",
+          duration: speed,
+          callback:()=>{
+              new Anime(_bottom[index],{
+                  prop:"width", 
+                  value:"100%",
+                  duration: speed,
+                  callback:()=>{
+                      new Anime(_left[index],{
+                          prop:"height", 
+                          value:"100%",
+                          duration: speed
+                      })
+                  } 
+              })
+          } 
+      })
+    }
+  })
+}
+
+function borderRemove(index){
+  new Anime(_left[index],{
+    prop:"height",
+    value:"0%",
+    duration: speed,
+    callback:()=>{
+
+      new Anime(_bottom[index],{
+          prop:"width", 
+          value:"0%",
+          duration: speed,
+          callback:()=>{
+
+              new Anime(_right[index],{
+                  prop:"height", 
+                  value:"0%",
+                  duration: speed,
+                  callback:()=>{
+
+                      new Anime(_top[index],{
+                          prop:"width", 
+                          value:"0%",
+                          duration: speed,
+                      })
+                  } 
+              })
+          } 
+      })
+    }
+  })
+}
+
+
+
