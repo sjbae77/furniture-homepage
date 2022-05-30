16cffe8127a41f09a163710847028d8d1a116eba
Bae Sujeong <doya60@gmail.com>
2022-05-30 23:50:06 +0900

    메인 - 반응형 80% 완료

--- js/main.js
+++ js/main.js
@@ -12,14 +12,6 @@ const mainSecs = document.querySelectorAll("section");
 const mainHd = document.querySelector(".main_hd");
 const secPosArr = [];
 
-const bannerSec = document.querySelector("#banner");
-const mainBannerList = bannerSec.querySelector(".list");
-const bannerPrev = bannerSec.querySelector(".prev");
-const bannerNext = bannerSec.querySelector(".next");
-let num = 0;
-let wid = 0;
-let timer = null;
-
 /* youtube
 key:AIzaSyCNEFP7grGD77zUQvYF6Tg93dOjeA-mCjs
 playlist:PLKoTiVSIVIvmIOTOJ5kgr1E14mbtOp1R4
@@ -37,13 +29,17 @@ const youtubeUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=sni
 const skipNav_item = document.querySelectorAll("#skipNav li a");
 
 
+for(let section of mainSecs){
+  secPosArr.push(section.offsetTop);
+}
+
 /* skipNav =================================================================*/
-for(let item of skipNav_item){
-  item.addEventListener("focusin", e=>{
+for (let item of skipNav_item) {
+  item.addEventListener("focusin", e => {
     item.classList.add("on");
   });
 
-  item.addEventListener("focusout", e=>{
+  item.addEventListener("focusout", e => {
     item.classList.remove("on");
   })
 }
@@ -53,23 +49,23 @@ const cookiePop = document.querySelector("#cookiePop");
 const btnCookiePopClose = cookiePop.querySelector(".close");
 const isCookie = document.cookie.indexOf("today=done");
 
-if(isCookie == -1){
+if (isCookie == -1) {
   cookiePop.style.display = "block";
-} else{
-  cookiePop.style.display ="none";
+} else {
+  cookiePop.style.display = "none";
 }
 
-btnCookiePopClose.addEventListener("click", ()=>{
+btnCookiePopClose.addEventListener("click", () => {
   let isChecked = cookiePop.querySelector("input[type=checkbox]").checked;
-  
-  if(isChecked) setCookie("today", "done", 1);
-  cookiePop.style.display ="none";
+
+  if (isChecked) setCookie("today", "done", 1);
+  cookiePop.style.display = "none";
 })
 
-function setCookie(cookieName, cookieValue, time){
+function setCookie(cookieName, cookieValue, time) {
   const today = new Date();
   const date = today.getDate();
-  today.setDate(date+time);
+  today.setDate(date + time);
 
   const duedate = today.toGMTString();
 
@@ -80,27 +76,27 @@ function setCookie(cookieName, cookieValue, time){
 /* about =================================================================*/
 createYoutubeList(youtubeUrl);
 
-function createYoutubeList(youtubeUrl){
+function createYoutubeList(youtubeUrl) {
   //url 요청
   fetch(youtubeUrl)
-  //요청한 url 받음(json 형태로)
-  .then(data=>{
-    return data.json();
-  })
-  .then(json=>{
-    let items = json.items;
-    // console.log(items);
+    //요청한 url 받음(json 형태로)
+    .then(data => {
+      return data.json();
+    })
+    .then(json => {
+      let items = json.items;
+      // console.log(items);
 
-    let result = '';
+      let result = '';
 
-    items.map(item=>{
+      items.map(item => {
 
-      let title = item.snippet.title;
-      if(title.length > 25) title = title.substr(0, 25)+"...";
-      let desc = item.snippet.description;
-      if(desc.length > 80) desc = desc.substr(0, 80)+"...";
+        let title = item.snippet.title;
+        if (title.length > 25) title = title.substr(0, 25) + "...";
+        let desc = item.snippet.description;
+        if (desc.length > 80) desc = desc.substr(0, 80) + "...";
 
-      result+=`
+        result += `
         <article>
           <div class="pic">
             <img src="${item.snippet.thumbnails.maxres.url}"/>
@@ -114,238 +110,69 @@ function createYoutubeList(youtubeUrl){
           </a>
         </article>
       `
-    });
+      });
 
-    youtube.innerHTML = result;
+      youtube.innerHTML = result;
 
-    createYoutubePop();
-  })
+      createYoutubePop();
+    })
 }
 
-document.body.addEventListener("click", e=> closecreateYoutubePop(e));
+document.body.addEventListener("click", e => closecreateYoutubePop(e));
 
-function createYoutubePop(){
+function createYoutubePop() {
 
   const youtubeBtn = document.querySelector(".youtubeBtn");
 
-    youtubeBtn.addEventListener("click",e=>{
-      e.preventDefault();
+  youtubeBtn.addEventListener("click", e => {
+    e.preventDefault();
 
-      const vidId = e.target.parentElement.getAttribute("data-vid");
+    const vidId = e.target.parentElement.getAttribute("data-vid");
 
-      let pop = document.createElement("aside");
-      pop.classList.add("commonPop");
-      pop.innerHTML = `
+    let pop = document.createElement("aside");
+    pop.classList.add("commonPop");
+    pop.innerHTML = `
       <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${vidId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <span>CLOSE</span>
       `;
 
-      document.body.append(pop);
+    document.body.append(pop);
 
-      new Anime(pop,{
-        prop:"opacity",
-        value:1,
-        duration:500
-      })
-
-      document.body.classList.add("on");
+    new Anime(pop, {
+      prop: "opacity",
+      value: 1,
+      duration: 500
     })
+
+    document.body.classList.add("on");
+  })
 }
 
-function closecreateYoutubePop(e){
+function closecreateYoutubePop(e) {
   const pop = document.querySelector(".commonPop");
 
-  if(pop){
+  if (pop) {
     const close = pop.querySelector("span");
-    if(e.target == close) {
+    if (e.target == close) {
       pop.remove();
       document.body.classList.remove("on");
     }
   }
 }
 
-/* banner =================================================================*/
-createBannerList("mainBannerData.json");
-
-timer = setInterval(move, 50);
-
-banner.addEventListener("mouseenter", ()=>{
-  clearInterval(timer);
-});
-banner.addEventListener("mouseleave", ()=>{
-  timer = setInterval(move, 50);
-})
-
-//동적으로 list 생성
-function createBannerList(url){
-  fetch(url)
-  .then(data => {
-    return data.json();
-  })
-  .then(json => {
-    let items = json.bannerImgSrc;
-    
-
-    let tags = '';
-
-    items.forEach(item => {
-      tags += `
-        <li><a href=${item.img}><img src=${item.img}><span>CLICK</span></a></li>
-      `;
-    })
-
-    mainBannerList.innerHTML = tags;
-
-    initList();
-  })
-  .catch(err=>{
-    console.log("데이터를 호출하는데 실패했습니다!");
-  })
-}
-
-//동적으로 생성된 ul 스타일 제어 함수 
-function initList() {
-  const list_li = mainBannerList.querySelectorAll("li");
-  const len = list_li.length;
-  wid = parseInt(getComputedStyle(list_li[0]).width);
-  mainBannerList.style.width = len * wid + "px";
-  mainBannerList.style.marginLeft = -wid + "px";
-  mainBannerList.prepend(mainBannerList.lastElementChild);
-}
-
-//num값을 list의 margin-left에 적용하여 앞으로 이동하는 함수 
-function move() {
-  //num값이 -480이 되면
-  if (num < -wid * 2) {
-    //num값을 -width 값으로 고정하고 
-    num = -wid;
-    //맨앞의 li를 list마지막으로 보냄 
-    mainBannerList.append(mainBannerList.firstElementChild);
-  } else {
-    //num값이 -(width값 * 2)가 되기 전까지는 2씩 빼줌 
-    num -= 2;
-  }
-  //num값을 list margin-left에 적용 
-  mainBannerList.style.marginLeft = num + "px";
-}
-
-bannerPrev.addEventListener("click", e=>{
-  e.preventDefault();
-
-  if(enableClick){
-    bannerPrevAni();
-    enableClick = false;
-  }
-})
-
-bannerNext.addEventListener("click", e=>{
-  e.preventDefault();
-
-  if(enableClick){
-    bannerNextAni();
-    enableClick = false;
-  }
-})
-
-//동적으로 팝업 생성
-mainBannerList.addEventListener("click", e=>{
-  const imgSrc = e.target.parentElement.getAttribute("href");
-  e.preventDefault();
-
-  createPop(imgSrc);
-})
-
-//동적으로 생성된 팝업이므로
-//이벤트 위임으로 팝업제거 이벤트 처리
-document.body.addEventListener("click", e=>{
-  removePop(e);
-})
-
-function createPop(imgSrc){
-  const pop = document.createElement("aside");
-  pop.classList.add("commonPop");
-
-  pop.innerHTML = `
-    <div class="pic">
-      <img src=${imgSrc}>
-    </div>
-    <span>CLOSE</span>
-  `;
-
-  document.body.append(pop);
-
-  new Anime(pop,{
-    prop:"opacity",
-    value:1,
-    duration:500
-  })
-
-  document.querySelector("body").classList.add("on");
-}
-
-function removePop(e){
-  const pop = document.querySelector("aside");
-  
-  //팝업이 생성된 상태라면
-  if(pop){
-    //span태그를 찾아서 close에 저장
-    const close = pop.querySelector("span");
-    
-    //클릭한 요소가 close일때 팝업 remove
-    if(e.target === close){
-      new Anime(pop,{
-        prop:"opacity",
-        value:0,
-        duration:500,
-        callback:()=>{
-          document.querySelector(".bannerPop").remove();
-          document.querySelector("body").classList.remove("on");
-        }
-      })
-    }
-  }
-}
-
-function bannerNextAni(){
-  new Anime(mainBannerList,{
-    prop:"margin-left",
-    value:-wid * 2,
-    duration: 500,
-    callback:()=>{
-      mainBannerList.append(mainBannerList.firstElementChild);
-      mainBannerList.style.marginLeft = -wid + "px";
-      enableClick = true;
-    }
-  })
-}
-
-function bannerPrevAni(){
-  new Anime(mainBannerList, {
-    prop:"margin-left",
-    value:0,
-    duration:500,
-    callback:()=>{
-      mainBannerList.prepend(mainBannerList.lastElementChild);
-      mainBannerList.style.marginLeft = -wid +"px";
-      enableClick = true;
-    }
-  })
-}
-
-
 /* news =====================================================================*/
 //뉴스 섹션 보더 이벤트
-newsArticles.forEach((el,index)=>{
+newsArticles.forEach((el, index) => {
   _unHover.push(false);
 
-  el.addEventListener("mouseenter",()=>{
+  el.addEventListener("mouseenter", () => {
     _unHover[index] = false;
 
     borderDraw(index);
   })
 
-  el.addEventListener("mouseleave",()=>{
-    if(_unHover[index] == false)
+  el.addEventListener("mouseleave", () => {
+    if (_unHover[index] == false)
       _unHover[index] = true;
 
     borderRemove(index);
@@ -353,8 +180,8 @@ newsArticles.forEach((el,index)=>{
 })
 
 function ani(_dir, _idx, _prop, _value, _dur, _call) {
-  if(_unHover[_idx] == true){
-    
+  if (_unHover[_idx] == true) {
+
     borderRemove(_idx);
     return;
   }
@@ -367,67 +194,67 @@ function ani(_dir, _idx, _prop, _value, _dur, _call) {
   })
 }
 
-function borderDraw(index){
-  newsArticles.forEach((el,idx)=>{
-    if (index != idx){
+function borderDraw(index) {
+  newsArticles.forEach((el, idx) => {
+    if (index != idx) {
       borderRemove(idx);
     }
   })
 
   ani(_top,
-      index, 
-      "width", 
+    index,
+    "width",
+    "100%",
+    300,
+    () => ani(_right,
+      index,
+      "height",
       "100%",
       300,
-      () => ani(_right,
-                index, 
-                "height",
-                "100%",
-                300,
-                () => ani(_bottom,     
-                          index, 
-                          "width",
-                          "100%",
-                          300,
-                          () => ani(_left,
-                                    index, 
-                                    "height",
-                                    "100%",
-                                    300
-                                    ))));
+      () => ani(_bottom,
+        index,
+        "width",
+        "100%",
+        300,
+        () => ani(_left,
+          index,
+          "height",
+          "100%",
+          300
+        ))));
 }
 
-function borderRemove(index){
-  new Anime(_left[index],{
-    prop:"height",
-    value:"0%",
+function borderRemove(index) {
+  new Anime(_left[index], {
+    prop: "height",
+    value: "0%",
     duration: borderSpeed,
   })
 
-  new Anime(_bottom[index],{
-      prop:"width", 
-      value:"0%",
-      duration: borderSpeed,
+  new Anime(_bottom[index], {
+    prop: "width",
+    value: "0%",
+    duration: borderSpeed,
   })
 
-  new Anime(_right[index],{
-      prop:"height", 
-      value:"0%",
-      duration: borderSpeed,
+  new Anime(_right[index], {
+    prop: "height",
+    value: "0%",
+    duration: borderSpeed,
   })
 
-  new Anime(_top[index],{
-      prop:"width", 
-      value:"0%",
-      duration: borderSpeed,
+  new Anime(_top[index], {
+    prop: "width",
+    value: "0%",
+    duration: borderSpeed,
   })
 
 }
 
 //스크롤이벤트 - 탑버튼 추가, 헤더 on으로 활성화
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", () => {
 
-  if(this.scrollY > secPosArr[1] -200) {
+  if (this.scrollY > secPosArr[1] - 200) {
     topBtn.classList.add("on");
     mainHd.classList.add("on");
   } else {
@@ -436,20 +263,20 @@ window.addEventListener("scroll", ()=>{
   }
 })
 
-topBtn.addEventListener("click",e=>{
+topBtn.addEventListener("click", e => {
   e.preventDefault();
 
-  new Anime(window,{
-    prop:"scroll",
-    value:0,
-    duration:300
+  new Anime(window, {
+    prop: "scroll",
+    value: 0,
+    duration: 300
   })
 })
 
 
 /* counter =================================================================*/
-window.addEventListener("scroll", ()=>{
-  if(this.scrollY > secPosArr[6] -800) {
+window.addEventListener("scroll", () => {
+  if (this.scrollY > secPosArr[6] - 800) {
     counter(".num1", 15, 2000);
     counter(".num2", 25, 2000);
     counter(".num3", 1143, 2000);
@@ -457,20 +284,20 @@ window.addEventListener("scroll", ()=>{
   }
 })
 
-function counter(el, target, time){
+function counter(el, target, time) {
   const item = document.querySelector(el);
   let current = parseInt(item.innerText);
   let count = target - current;
-  let interval = time/count;
+  let interval = time / count;
 
-  let timer = setInterval(()=>{
-    if(current === target){
+  let timer = setInterval(() => {
+    if (current === target) {
       clearInterval(timer);
       return;
     }
 
-  current++;
-  item.innerText = current;
+    current++;
+    item.innerText = current;
 
-  },interval)
+  }, interval)
 }
